7a5fcf69c31ea727045b60d773fc17a50be30f5b
mokasousa <moka.sousa@gmail.com>
2019-08-20 08:32:11 -0300

    Protótipo HTML com CSS sem responsividade e comentários em JS sobre passo-a-passo das funções

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,27 @@
-//função da cifrar e decifrar
+//Javascript para as funções de codificar e decofificar
 
+/*Passo a passo de cada function:
+
+1. Apagar #answer-box
+2. Captar os valores:
+    offset = .cipher-header input value
+    string = #crypto-box textarea value
+3. Manipular a string (for loop?)... Algo como:
+    var stringEncode = ''
+      string.charCodeAt(i) -> retorna o códigoASC[i] para cada letra
+      novocódigo[i] = ((códigoASC - 65 + offset) % 26)+65 -> shift o códigoASC de cada letra
+      novaLetra[i] = String.fromCharCode(novocódigo[i])
+      add novaLetra[i] em stringEncode
+      return stringEncode
+4. Printar mensagem padrão em negrito e stringEncode em #answer-box
+*/
+
+/*
 window.cipher = {
-  //cipher.encode(offset, string){}
-  //cipher.decode(offset, string){}
-};
+  cipher.encode(offset, string){
+    console.log(offset, string);
+  }
+  cipher.decode(offset, string){
+    console.log(offset, string);
+  }
+};*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//EVENTOS DO DOM: Javascript para chamar as funções relacionados aos eventos de
+//click nos botões Cifrar e Decifrar
+
 /*
 function clickCifrar (){
   window.cipher.encode();
@@ -7,3 +10,5 @@ function clickDecifrar (){
   window.cipher.decode();
 }
 */
+
+//Assim dá? document.getElementById('encode-btn').addEventListener('click', window.cipher.encode);
