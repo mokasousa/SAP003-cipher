15e513aef0c76e4a09ca56ee95ee11122345b211
mokasousa <moka.sousa@gmail.com>
2019-08-20 15:12:50 -0300

    Função encode com maiúculas e botão Cifrar responsivo

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,45 @@
 //Javascript para as funções de codificar e decofificar
 
-/*Passo a passo de cada function:
+//Passo a passo da function Encode:
 
-1. Apagar #answer-box
-2. Captar os valores:
-    offset = .cipher-header input value
-    string = #crypto-box textarea value
-3. Manipular a string (for loop?)... Algo como:
-    var stringEncode = ''
-      string.charCodeAt(i) -> retorna o códigoASC[i] para cada letra
-      novocódigo[i] = ((códigoASC - 65 + offset) % 26)+65 -> shift o códigoASC de cada letra
-      novaLetra[i] = String.fromCharCode(novocódigo[i])
-      add novaLetra[i] em stringEncode
-      return stringEncode
-4. Printar mensagem padrão em negrito e stringEncode em #answer-box
-*/
+window.cipher = {
+  encode: encode,
+  //decode: decode
+};
 
+function encode(){
+
+//1. Apagar #answer-box e offset ao iniciar
+
+//2. Captar os valores:
+
+//let offset, stringInput
+
+//offset = Number(document.getElementById('offset').value);
+//stringInput = document.getElementById('input-box').value;
+console.log(offset);
+console.log(stringInput);
+
+let stringEncode = ''
+
+//3. Manipular a string/trocar as letras
+position = stringInput.length
+console.log(position);
+
+for (i=0; i<position; i++) {
+  let shiftedASC
+  console.log("Original: " + stringInput[i] + ',' + stringInput.charCodeAt(i)); // -> retorna o códigoASC[i] para cada letra
+  shiftedChar = String.fromCharCode(((stringInput.charCodeAt(i)- 65 + offset) % 26) + 65);
+  console.log("Shifted: " + shiftedChar);
+  stringEncode += shiftedChar;
+}
+console.log("Final: " + stringEncode);
+
+//4. Printar mensagem padrão em negrito e stringEncode em #answer-box
+document.getElementById('output-box').innerHTML = "Resultado: " + stringEncode
+
+console.log(offset, stringInput);
+}
 /*
 window.cipher = {
   cipher.encode(offset, string){
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 //EVENTOS DO DOM: Javascript para chamar as funções relacionados aos eventos de
 //click nos botões Cifrar e Decifrar
 
-/*
-function clickCifrar (){
-  window.cipher.encode();
+
+function clickCifrar() {
+  offset = Number(document.getElementById('offset').value);
+  stringInput = document.getElementById('input-box').value;
+  window.cipher.encode(offset, stringInput);
 }
 
+/*
 function clickDecifrar (){
   window.cipher.decode();
 }
