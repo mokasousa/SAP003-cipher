c7ae4e9f0e4372c3bc81da65ca0560a734a400f2
mokasousa <moka.sousa@gmail.com>
2019-08-26 13:20:39 -0300

    acerto dos botões e atualização para ES6

--- src/index.js
+++ src/index.js
@@ -4,20 +4,33 @@
 //definir como parâmetros os valores inseridos pelo usuário e chamar a função Encode
 
 document.getElementById("encode-btn").addEventListener("click", function clickCipher() {
-  let offset, stringInput;
-  offset = parseInt(document.getElementById("offset").value);
-  stringInput = document.getElementById("input-box").value;
-  const resultEncode = "Pronto, guarde com carinho! \n\n";
-  document.getElementById("output-box").innerHTML = resultEncode + window.cipher.encode(offset, stringInput);
+
+  //guardar os valores inseridos pelo usuário:
+  const offset = Math.abs(parseInt(document.getElementById("offset").value));
+  let stringInput = document.getElementById("input-box").value;
+
+  //Checar se o usuário escreveu no campo certo e escolheu um número válido:
+  offset.required;
+  stringInput.required;
+
+  if (offset && stringInput) {
+
+    //chamar a função encode e printar na caixa de resultado:
+    const resultEncode = "Pronto, guarde com carinho! \n\n";
+    document.getElementById("output-box").innerHTML = resultEncode + window.cipher.encode(offset, stringInput);
+  }
 });
 
 //Chamar a função clickCifrar ao evento de Click no botão Decifrar ->
 //definir como parâmetros os valores inseridos pelo usuário e chamar a função Encode
 
 document.getElementById("decode-btn").addEventListener("click", function clickDecipher() {
-  let offset, stringInput;
-  offset = parseInt(document.getElementById("offset").value);
-  stringInput = document.getElementById("input-box").value;
-  const resultDecode = "Desfrute e surpreenda a todos! \n\n";
-  document.getElementById("output-box").innerHTML = resultDecode + window.cipher.decode(offset, stringInput);
+  const offset = Math.abs(parseInt(document.getElementById("offset").value));
+  let stringInput = document.getElementById("input-box").value;
+  offset.required;
+  stringInput.required;
+  if (offset && stringInput) {
+    const resultDecode = "Desfrute e surpreenda a todos! \n\n";
+    document.getElementById("output-box").innerHTML = resultDecode + window.cipher.decode(offset, stringInput);
+  }
 });
