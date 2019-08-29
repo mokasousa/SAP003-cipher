7b2ae28bd051a5e48336053096e4b8a2e4c933cf
mokasousa <moka.sousa@gmail.com>
2019-08-29 15:41:14 -0300

    atualização de readme e testes unitários cobrem stmts, functions e branches

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Cifra de César
 
+## Índice
+
+* [1. Título do projeto](#1-titulo-do-projeto)
+* [2. Projeto](#2-projeto)
+* [3. Funcionamento](#3-funcionamento)
+* [4. Autores](#4-autores)
+* [5. Considerações](#5-consideracoes)
+
+<a name="1-titulo-do-projeto"></a>
 ## Título do projeto
 
 NESTLETOULOUSE: Crie uma criptografia para sua receita secreta!
+
+<a name="2-projeto"></a>
+## Projeto
 
 Nesta aplicação web o usuário poderá cifrar e decifrar uma receita indicando uma
 chave de deslocamento. A criptografia utilizada é a chamada Cifra de César que
@@ -13,13 +25,14 @@ Esta aplicação surgiu da necessidade de manter o segredo de receitas que podem
 ser verdadeiras heranças passadas de geração em geração dentro da família.
 Assim, o usuário é capaz cifrar uma receita que deseja tornar ilegível e,
 portanto, mais segura. Depois, quando o usuário desejar utilizá-la novamente, é
-decifrar a receita sabendo apenas a chave de deslocamento que foi utilizada na
-cifra.
+possível decifrar a receita sabendo apenas a chave de deslocamento que foi
+utilizada para cifrá-la.
 A aplicação possui uma chave para escolha do número de deslocamento, uma caixa
-onde o usuário é capaz de escrever a receita e uma caixa onde será escrito o
+onde o usuário é capaz de escrever a receita e uma caixa onde será devolvido o
 resultado da criptografia.
 O desenho, o nome e as cores da aplicação foram inspiradas na série Friends.
 
+<a name="3-funcionamento"></a>
 ## Funcionamento
 
 Ao fazer download da aplicação web o usuário entrará na página principal onde
@@ -27,11 +40,13 @@ haverá um campo indicando a escolha de um número de deslocamento. Esse número
 pode ser negativo ou positivo. Abaixo haverá duas caixas de texto, uma à esquerda onde será inserido a receita que se deseja codificar ou decodificar e outra, à direita, onde será retornado o resultado após o click nos botões abaixo, Cifrar e Decifrar. O botões só funcionarão caso o usuário escreva nos
 dois campos obrigatórios.
 
+<a name="4-autores"></a>
 ## Autores
 
 * Mônica Sousa - HTML, CSS e JavaScript
 * Laboratória SP
 
+<a name="5-consideracoes"></a>
 ## Considerações
 
 Inspiração: Friends, temporada 7, episódio 3.
--- src/cipher.js
+++ src/cipher.js
@@ -7,16 +7,13 @@ window.cipher = {
 
 function encode(offset, stringInput) {
 
-  let shiftedString, charCode, code, shiftedChar;
+  let shiftedString, charCode, shiftedChar;
 
   //transformar a string em um array e mapear cada item executando uma função:
   shiftedString = Array.from(stringInput).map((char) => {
 
     //Associar o número ASC para cada letra do array:
-    code = char.charCodeAt();
-
-    //função para trocar letras com acentos para letras sem acentos:
-    charCode = accentCheck(code);
+    charCode = char.charCodeAt();
 
     //Condição trocar letras Maiúsculas:
     if (65 <= charCode && charCode <= 90) {
@@ -42,10 +39,9 @@ function encode(offset, stringInput) {
 }
 
 function decode(offset, stringInput) {
-  let shiftedString, charCode, code, shiftedChar;
+  let shiftedString, charCode, shiftedChar;
   shiftedString = Array.from(stringInput).map((char) => {
-    code = char.charCodeAt();
-    charCode = accentCheck(code);
+    charCode = char.charCodeAt();
     if (65 <= charCode && charCode <= 90) {
       shiftedChar = String.fromCharCode(((charCode + 65 - offset) % 26) + 65);
     } else if (97 <= charCode && charCode <= 122) {
@@ -57,124 +53,3 @@ function decode(offset, stringInput) {
   });
   return shiftedString.join("");
 }
-
-//função para trocar letras com acentos para letras sem acentos:
-function accentCheck (code) {
-  switch (code) {
-  case 199:
-    code = 67;
-    break;
-  case 231:
-    code = 99;
-    break;
-  case 233:
-  case 234:
-    code = 101;
-    break;
-  case 201:
-  case 202:
-    code = 69;
-    break;
-  case 224:
-  case 225:
-  case 226:
-  case 227:
-    code = 97;
-    break;
-  case 192:
-  case 193:
-  case 194:
-  case 195:
-    code = 65;
-    break;
-  case 237:
-    code = 105;
-    break;
-  case 205:
-    code = 73;
-    break;
-  case 243:
-  case 244:
-  case 245:
-    code = 111;
-    break;
-  case 211:
-  case 212:
-  case 213:
-    code = 79;
-    break;
-  case 250:
-    code = 117;
-    break;
-  case 218:
-    code = 85;
-    break;
-  default:
-    break;
-  }
-  return code;
-}
-
-//----------------------------------------------------------------------------//
-//OUTRA FORMA DE RESOLVER: USANDO FOR LOOP...
-
-/*
-function encode(offset, stringInput) {
-
-  let position, stringEncode, shiftedChar, eachCharCode;
-
-  //Checar se o usuário escreveu no campo certo e escolheu um número válido:
-  if (offset !== "" && offset !== 0 && stringInput !== "") {
-
-  //Definir o tamanho da string e declarar uma nova string inicialmente vazia:
-  position = stringInput.length;
-
-  stringEncode = "";
-
-    //Loop para trocar as letras da string:
-    for (let i = 0; i < position; i++) {
-
-    //Associar o número ASC para cada letra da string:
-    eachCharCode = stringInput.charCodeAt(i);
-
-    //Condição trocar letras Maiúsculas:
-    if (65 <= eachCharCode && eachCharCode <= 90) {
-
-      //Trocar o número ASC de acordo com offset e associá-lo a letra correspondente:
-      shiftedChar = String.fromCharCode(((eachCharCode - 65 + offset) % 26) + 65);
-
-      //Adicionar a letra à nova string a ser formada:
-      stringEncode += shiftedChar;
-
-    //Condição trocar letras minúsculas:
-    } else if (97 <= eachCharCode && eachCharCode <= 122) {
-      shiftedChar = String.fromCharCode(((eachCharCode - 97 + offset) % 26) + 97);
-      stringEncode += shiftedChar;
-
-    //Condição para não alterar outros caracteres da string:
-    } else {
-      stringEncode += stringInput.charAt(i);
-    }
-  }
-  return stringEncode;
-};
-
-function decode(offset, stringInput) {
-  let position, stringDecode, shiftedChar, eachCharCode;
-  position = stringInput.length;
-  stringDecode = "";
-  for (let i = 0; i < position; i++) {
-    eachCharCode = stringInput.charCodeAt(i);
-    if (65 <= eachCharCode && eachCharCode <= 90) {
-      shiftedChar = String.fromCharCode(((eachCharCode + 65 - offset) % 26) + 65);
-      stringDecode += shiftedChar;
-    } else if (97 <= eachCharCode && eachCharCode <= 122) {
-      shiftedChar = String.fromCharCode((122 - (122 - eachCharCode + offset) % 26));
-      stringDecode += shiftedChar;
-    } else {
-      stringDecode += stringInput.charAt(i);
-    }
-  }
-  return stringDecode;
-};
-*/
